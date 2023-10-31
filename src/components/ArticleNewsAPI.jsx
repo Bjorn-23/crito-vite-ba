15997a7bba3532c114528ddeee095afd122847ce
Bjorn-23 <bjornandersson84@gmail.com>
2023-10-31 14:27:45 +0100

    added articles in news via API

--- src/components/ArticleNewsAPI.jsx
+++ src/components/ArticleNewsAPI.jsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from 'react'
-//import ArticlesBox from './generics/ArticlesBox';
+import { Link } from 'react-router-dom'
 import SectionTitle from './generics/SectionTitle'
+
 
 
 
 const ArticleNewsAPI = () => {
 
-    let firstName = Hans
+      const [articles, setArticles] = useState([])
+    //   console.log(articles[0].id)
 
+    const getArticles = async () => {
+        const result = await fetch('https://win23-assignment.azurewebsites.net/api/articles')
+        // const data = await result.json()
+        // setArticles(data) FÖRENKLAR NER TILL NÄSTA RAD
+        setArticles(await result.json())
+    }
 
+    useEffect(() => {
+        getArticles()
+        console.log('articles fetched safely')
+    }, [])
 
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         getArticles()
+    //         console.log('articles fetched')
+    //     }, 60000)
+
+    //     getArticles()
+    //     console.log('articles fetched onMount')
+
+    //     return () => clearInterval(interval)
+    // }, [])
 
     return (
         <section className="articles-news">
@@ -29,7 +52,30 @@ const ArticleNewsAPI = () => {
                 </div>
 
                 <div className="middle grid-container">
-                    
+                
+                    {
+                    articles.map(article => (
+                    <Link key={article.id} className="grid-item" to='#'>
+                        <div className="img-wrapper">
+                            <img className="article-img relative" src={article.imageUrl}
+                                alt='altText' />
+                            <div className="date-wrapper">
+                                <div className="yellow-square absolute">   
+                                <div className="date">123</div>           
+                                <p className="month">Mar</p>           
+                                    {/* <div className="date">{output.split(" ")[0]}</div>
+                                    <p className="month">{output.split(" ")[1]}</p> */}
+                                </div>
+                            </div>
+                        </div>
+                            <p className="subject">{article.category}</p>
+                            <h3 className="article-title">{article.title}</h3>
+                            <p className="lorem">{article.content}</p>
+                    </Link>
+                    ))
+
+                    }
+
                 </div>
 
                 <div className="bottom">
--- src/views/News.jsx
+++ src/views/News.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 import Header from '@components/Header'
 import Breadcrumbs from '@components/Breadcrumbs'
-import ArticlesNews from '@components/ArticlesNews'
+// import ArticlesNews from '@components/ArticlesNews'
+import ArticleNewsAPI from '../components/ArticleNewsAPI'
 import Footer from '@components/Footer'
 
 const News = () => {
@@ -10,7 +11,8 @@ const News = () => {
     <div className="wrapper">
       <Header />
       <Breadcrumbs url="/News" page="News" />
-      <ArticlesNews />
+      {/* <ArticlesNews /> */}
+      <ArticleNewsAPI />
       <Footer />
     </div>
 
