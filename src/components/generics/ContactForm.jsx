8a2eaac92e59fd4cecf511f3f9e0602924c854ae
Bjorn-23 <bjornandersson84@gmail.com>
2023-11-07 12:47:47 +0100

    contactform ready for validation

--- src/components/generics/ContactForm.jsx
+++ src/components/generics/ContactForm.jsx
@@ -1,32 +1,72 @@
-import React from 'react'
+import { Formik, useFormik } from 'formik'
+import React, { useState } from 'react'
 
 const ContactForm = () => {
 
+    const [errorMessage, setErrorMessage] = useState('')
+ 
+    const form = useFormik({
 
+        initialValues: {
+            name: "",
+            email: "",
+            message: "",
+            terms: false,
 
+        },
+
+        onSubmit: async (values) => {
+
+            const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+                method: 'post',
+                headers: {
+                    'content-type' : 'application/json'
+                },
+                body: JSON.stringify(values)
+               
+            })
+
+            switch (result.status) {
+                case 200:
+                    form.resetForm()
+                    alert('Message sent')
+                    setErrorMessage('')
+                    break;
+                case 400:
+                    setErrorMessage('Invalid input - please make sure all fields are filled in')
+                    break;
+            }
+        }
+    })
 
     return (
 
         <div className="message-us" id="#message-us">
             <h2 className="message-title">Leave us a message for any information.</h2>
 
-            <form noValidate>
-                <p className="error-message"></p>
+            <form onSubmit={form.handleSubmit} noValidate>
+                <p className="errorMessage">{errorMessage}</p>
                 <div>
-                    <input id="name" type="text" name="name" placeholder="Your Full Name...*" required />
+                    <input type="text" name="name" placeholder="Your Full Name...*" value={form.values.name} onChange={form.handleChange} required />
                     <span id="name-error"></span>
                 </div>
 
                 <div>
-                    <input id="email" type="email" name="email" placeholder="Your E-mail...*" required />
+                    <input type="email" name="email" placeholder="Your E-mail...*" value={form.values.email} onChange={form.handleChange} required />
                     <span id="email-error"></span>
                 </div>
 
                 <div>
-                    <textarea id="message" type="text" rows="5" name="message" placeholder="Your Message...*" required />
-                    <span id="message-error"></span></div>
+                    <textarea type="text" rows="5" name="message" placeholder="Your Message...*" value={form.values.message} onChange={form.handleChange} required />
+                    <span id="message-error"></span>
+                </div>
 
-                <button id="c_submit" type="submit" className="submit btn-yellow">Send Message<i
+                <div>
+                    <input type="checkbox" name="terms" checked={form.values.terms} onChange={form.handleChange} required />
+                    <span id="terms-error"></span>
+                </div>
+
+                <button type="submit" disabled={!form.values.terms} className="submit btn-yellow">Send Message<i
                     className="fa-solid fa-arrow-up-right"></i></button>
             </form>
         </div>
